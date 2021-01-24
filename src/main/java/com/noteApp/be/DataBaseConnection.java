43db05a42bcd072c390d99675a685d58835de4b0
elia404 <elia404@gmail.com>
2021-01-24 13:59:15 +0200

    git push

--- src/main/java/com/noteApp/be/DataBaseConnection.java
+++ src/main/java/com/noteApp/be/DataBaseConnection.java
@@ -1,8 +1,6 @@
 package com.noteApp.be;
 
-import com.noteApp.ui.NewNote;
-import com.sun.xml.internal.bind.v2.model.core.ID;
-
+import javax.sql.DataSource;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +9,15 @@ public class DataBaseConnection {
     int noteId;
     Connection c = null;
     Statement stmt = null;
+    ConnectionPool jdbcObj = new ConnectionPool();
     String noteText;
-    List<Note> allNotes = new ArrayList<>();
+    List<Note> allNotes = new ArrayList<Note>();
 
     public List select() {
 
         try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
-                            "postgres", "noham");
+            DataSource dataSource = jdbcObj.setUpPool();
+            c = dataSource.getConnection();
             System.out.println("Opened database successfully");
             stmt = c.createStatement();
             ResultSet resultSet = stmt.executeQuery("select * from notes;");
@@ -32,29 +29,22 @@ public class DataBaseConnection {
                 Note n = new Note(noteText);
                 n.setId(noteId);
                 allNotes.add(n);
-
-
             }
             resultSet.close();
             stmt.close();
             c.close();
 
-
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
-
         }
         return allNotes;
-
     }
 
 
     public Note selectOne() {
         try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
-                            "postgres", "noham");
+            DataSource dataSource = jdbcObj.setUpPool();
+            c = dataSource.getConnection();
             System.out.println("Opened database successfully");
             stmt = c.createStatement();
             ResultSet resultSet = stmt.executeQuery("select * from notes where true;");
@@ -68,63 +58,46 @@ public class DataBaseConnection {
             resultSet.close();
             stmt.close();
             c.close();
-
-
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
-
         return new Note(noteText);
     }
 
-
     public void insertNote(String text) {
         try {
-
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
-                            "postgres", "noham");
+            DataSource dataSource = jdbcObj.setUpPool();
+            c = dataSource.getConnection();
             System.out.println("Opened database successfully");
             stmt = c.createStatement();
             ResultSet resultSet = stmt.executeQuery("INSERT INTO public.notes(id, text) VALUES(default,'"+text+"') returning id");
-
             while (resultSet.next()) {
                 Note n = new Note(text);
                 n.getId();
                 n.getText();
                 resultSet.getMetaData();
-
             }
-//
             resultSet.close();
             stmt.close();
             c.close();
-
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
-
-
 
     }
     public void update(int id, String newText) {
         try {
 
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
-                            "postgres", "noham");
+            DataSource dataSource = jdbcObj.setUpPool();
+            c = dataSource.getConnection();
             System.out.println("Opened database successfully");
             stmt = c.createStatement();
             ResultSet resultSet = stmt.executeQuery("UPDATE public.notes SET text = '"+newText+"' WHERE id = '"+id+"' returning text" );
-
             while (resultSet.next()) {
                 Note n = new Note(newText);
                 n.setText(newText);
                 n.getId();
                 resultSet.getMetaData();
-
             }
             resultSet.close();
             stmt.close();
@@ -134,18 +107,13 @@ public class DataBaseConnection {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
         }
 
-
-
     }
-
 
     public void deleteOne(int id){
         try {
 
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/postgres",
-                            "postgres", "noham");
+            DataSource dataSource = jdbcObj.setUpPool();
+            c = dataSource.getConnection();
             System.out.println("Opened database successfully");
             stmt = c.createStatement();
             ResultSet resultSet = stmt.executeQuery("DELETE FROM public.notes where id= '"+id+"' returning id");
