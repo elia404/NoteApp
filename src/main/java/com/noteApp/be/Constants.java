7131f50347ca669c0ff6074e94308b8a73589155
elia404 <elia404@gmail.com>
2021-01-27 14:33:02 +0200

    git push

---
+++ src/main/java/com/noteApp/be/Constants.java
@@ -0,0 +1,8 @@
+package com.noteApp.be;
+
+public class Constants {
+    public static final int FONT_SIZE = 20;
+    public static final String FONT_STYLE ="Arial";
+    public static final String ID_COL = "id";
+    public static final String TEXT_COL = "text";
+}
